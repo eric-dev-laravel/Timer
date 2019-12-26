4468223beb1627befa4d0f7a43f7745ec18e1552
eric-dev-laravel <54290874+eric-dev-laravel@users.noreply.github.com>
2019-12-26 17:46:41 -0600

    GPS get location users

--- app/src/main/java/com/sepankasuite/timer/MainActivity.java
+++ app/src/main/java/com/sepankasuite/timer/MainActivity.java
@@ -14,6 +14,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -68,6 +69,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.btn_timer:
                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
                     return;
                 }
                 Location location = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
